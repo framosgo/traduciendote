2f576b1d9a30ece8bf16aadcf4d435045e0c76bb
Magaz Merino <708055@cognizant.com>
2019-03-14 15:08:27 +0100

    Add Husky dependencie

--- src/app/containers/App/components/Sidebar/index.jsx
+++ src/app/containers/App/components/Sidebar/index.jsx
@@ -1,14 +1,6 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { ROUTES } from 'routes/routes';
-import Accordion from 'components/Accordion';
 
 import styles from './styles';
-
-const activeStyle = {
-  fontWeight: 'bold',
-  color: 'rgb(0, 122, 255)'
-};
 
 class Sidebar extends Component {
   state = {
@@ -25,14 +17,12 @@ class Sidebar extends Component {
     const { isOpen } = this.state;
     const iconClass = isOpen ? styles.chevronLeft : styles.chevronRight;
     const navClass = isOpen ? '' : styles.navCollapsed;
-    const leches;
     return (
       <nav className={ navClass }>
         <div className={ styles.collapserIcon } onClick={ this.toogle }>
           <div className={ iconClass } />
           <div className={ iconClass } />
         </div>
-  
       </nav>
     );
   }
