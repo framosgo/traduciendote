b142de315f37dc8e3d52468661e85803f48555f7
Magaz Merino <708055@cognizant.com>
2019-03-14 15:05:40 +0100

    Update dependencies and add Husky & css-modules

--- src/app/containers/App/components/Sidebar/index.jsx
+++ src/app/containers/App/components/Sidebar/index.jsx
@@ -25,58 +25,14 @@ class Sidebar extends Component {
     const { isOpen } = this.state;
     const iconClass = isOpen ? styles.chevronLeft : styles.chevronRight;
     const navClass = isOpen ? '' : styles.navCollapsed;
+    const leches;
     return (
       <nav className={ navClass }>
         <div className={ styles.collapserIcon } onClick={ this.toogle }>
           <div className={ iconClass } />
           <div className={ iconClass } />
         </div>
-        {isOpen && (
-          <Accordion
-            allowMultipleOpen={ true }
-            className={ styles.accordion }
-            openIconClass={ styles.chevronDown }
-            closeIconClass={ styles.chevronRight }
-            noIcon={ true }
-          >
-            <div label="USERS ADMINISTRATION" className={ styles.section }>
-              <div className={ styles.subNav }>
-                <NavLink to={ ROUTES.PRIVATE.USERS.ROOT } activeStyle={ activeStyle }>
-                  Users
-                </NavLink>
-                <NavLink to={ ROUTES.PRIVATE.ROLES.ROOT } activeStyle={ activeStyle }>
-                  Roles
-                </NavLink>
-                <NavLink to={ ROUTES.PRIVATE.PERMITS } activeStyle={ activeStyle }>
-                  Permits
-                </NavLink>
-                <NavLink to={ ROUTES.PRIVATE.MENU_OPTIONS } activeStyle={ activeStyle }>
-                  Menu Options
-                </NavLink>
-              </div>
-            </div>
-            <div label="LOREM IPSUM" className={ styles.section }>
-              <div className={ styles.subNav }>
-                <NavLink to={ ROUTES.PRIVATE.USERS.ROOT } activeStyle={ activeStyle }>
-                  Users
-                </NavLink>
-                <NavLink to={ ROUTES.PRIVATE.ROLES.ROOT } activeStyle={ activeStyle }>
-                  Roles
-                </NavLink>
-              </div>
-            </div>
-            <div label="LOREM IPSUM DOLOR" className={ styles.section }>
-              <div className={ styles.subNav }>
-                <NavLink to={ ROUTES.PRIVATE.USERS.ROOT } activeStyle={ activeStyle }>
-                  Users
-                </NavLink>
-                <NavLink to={ ROUTES.PRIVATE.ROLES.ROOT } activeStyle={ activeStyle }>
-                  Roles
-                </NavLink>
-              </div>
-            </div>
-          </Accordion>
-        )}
+  
       </nav>
     );
   }
