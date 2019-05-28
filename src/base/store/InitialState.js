0a3b0108a9b1cb42579f3caca2e6f002ffcfb688
Magaz Merino <708055@cognizant.com>
2019-05-28 14:36:39 +0200

    rename to ts and remove isomorfic initial state

--- src/base/store/InitialState.js
+++
@@ -1,8 +0,0 @@
-// FIXME TO TS
-
-import * as models from '../models';
-import setGlobalState from './globalState';
-
-export const initialState = window.$REACTBASE_STATE || {};
-
-setGlobalState(initialState, models);
