41b76a881953939573fe929e832ace8116253baf
José Manuel Bermudo <josemanuel.bermudodelgado@cognizant.com>
2019-05-28 15:20:18 +0200

    fixed globalState

---
+++ src/base/store/globalState.ts
@@ -0,0 +1,5 @@
+/* eslint-disable */
+const setGlobalState = (initialState: any, models: any): any =>
+  models.default.modelIndex.map((model: any): any => model.setInitialState(initialState));
+
+export default setGlobalState;
