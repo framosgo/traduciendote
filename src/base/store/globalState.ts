b680fa8e6e204b2a1bd3afbab89cc80cbadbb27e
José Manuel Bermudo <josemanuel.bermudodelgado@cognizant.com>
2019-05-28 15:25:42 +0200

    fixed export default

--- src/base/store/globalState.ts
+++ src/base/store/globalState.ts
@@ -1,5 +1,3 @@
 /* eslint-disable */
-const setGlobalState = (initialState: any, models: any): any =>
+export const setGlobalState = (initialState: any, models: any): any =>
   models.default.modelIndex.map((model: any): any => model.setInitialState(initialState));
-
-export default setGlobalState;
--- src/base/store/initialState.ts
+++ src/base/store/initialState.ts
@@ -1,5 +1,5 @@
 import * as models from '../models';
-import setGlobalState from './globalState';
+import { setGlobalState } from './globalState';
 
 export const initialState = {};
 
