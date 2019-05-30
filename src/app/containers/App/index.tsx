a63cb031bacd58ad0a21cb912b79580626ba9080
José Manuel Bermudo <josemanuel.bermudodelgado@cognizant.com>
2019-05-30 12:34:08 +0200

    fixed router lazy component

--- src/app/containers/App/index.tsx
+++ src/app/containers/App/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
+import { Routes } from '../../routes';
 
-const App: React.FunctionComponent = () => {
-  return <div>App</div>;
-};
+const App: React.FunctionComponent = () => <Routes />;
 
 export default App;
