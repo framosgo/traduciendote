9ae018a5fd3091baaf355b02c85f821e819f5001
Alberto Martin <alberto.martintorrejon@cognizant.com>
2019-06-05 19:19:29 +0200

    Add redux dev tools and add types for module.hot

--- src/base/store/configureStore.ts
+++ src/base/store/configureStore.ts
@@ -1,9 +1,9 @@
-/* eslint-disable global-require */
 /* eslint-disable */
 import { createLogger } from 'redux-logger';
 import reduxReqMiddleware from 'redux-req-middleware';
 import { createStore, applyMiddleware, Store, Action } from 'redux';
 // FIXME (path)
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { env } from '../shared/env';
 import { AppState } from '../../app/types/index';
 import { rootReducer } from '../reducers/index';
@@ -18,12 +18,9 @@ export const configureStore = (initialState: AppState): Store<{}, Action> => {
   } else {
     middleware = applyMiddleware(reduxReqMiddleware());
   }
-
-  const store = createStore(rootReducer, initialState, middleware);
-
+  const store = createStore(rootReducer, initialState, composeWithDevTools(middleware));
   if (module.hot) {
     module.hot.accept('../reducers', () => {
-      /* eslint global-require: "error" */
       const nextRootReducer = require('../reducers');
 
       store.replaceReducer(nextRootReducer);
